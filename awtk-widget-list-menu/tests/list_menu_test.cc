bfc5686356f864e1e78fbb1fcd4da40dda92346a
chentan <chentan@zlg.cn>
2020-08-26 17:07:24 +0800

    delete list-menu for fix bugs

--- awtk-widget-list-menu/tests/list_menu_test.cc
+++
@@ -1,9 +0,0 @@
-﻿#include "list_menu/list_menu.h"
-#include "gtest/gtest.h"
-
-TEST(list_menu, basic) {
-  value_t v;
-  widget_t* w = list_menu_create(NULL, 10, 20, 30, 40);
-
-  widget_destroy(w);
-}
